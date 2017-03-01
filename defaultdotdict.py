d3b537c2c7b4a29a7e3b546e56844908a4a8c96b
Terry N. Brown <terrynbrown@gmail.com>
2017-03-01 09:59:48 -0600

    comments etc.

--- defaultdotdict.py
+++ defaultdotdict.py
@@ -1,7 +1,20 @@
+"""
+DefaultDotDict - Yet another dictionary with dot notation.
+
+Supports loading from JSON.  Acts as defaultdict recursively
+adding more DefaultDotDicts when non-existent items are requested.
+
+See main() for a demo.
+
+Terry N. Brown, terrynbrown@gmail.com, Wed Mar 01 09:44:29 2017
+"""
+
 import json
 
 class DefaultDotDict(dict):
+    """Allow a.x as well as a['x'] for dicts"""
     def __getattr__(self, item):
+        # return the item or an empty DefaultDotDict
         if item not in self:
             self[item] = DefaultDotDict()
         return self[item]
@@ -9,24 +22,38 @@ class DefaultDotDict(dict):
         self[key] = value
     @staticmethod
     def json_object_hook(dct):
+        """for JSON's object_hook argument, convert dicts to DefaultDotDicts"""
         return DefaultDotDict(dct)
     @staticmethod
     def json_load(fileobj):
+        """used like json.load, but uses DefaultDotDict.json_object_hook"""
         return json.load(fileobj, object_hook=DefaultDotDict.json_object_hook)
 
 def main():
+    """simple test / demo of DefaultDotDict"""
     import os, pprint, tempfile
     a = DefaultDotDict(o=1)
+    a.update({'ans': 42})
     a.b[2] = [1, 2]
-    a.c.d.e = 'f'
+    a.c.d.e = 'nested'
+    # save to a tempory JSON file
     handle, filename = tempfile.mkstemp()
     os.close(handle)
     json.dump(a, open(filename, 'wb'))
     new_a = DefaultDotDict.json_load(open(filename))
     os.unlink(filename)
-    new_a.x.y = 'z'
+
+    # loaded as nested DefaultDotDict, not dict
+    new_a.point.x = 1.2
+    new_a.point.y = 2.1
+
+    # note keys converted to unicode (Python 2) and difference between
+    # key in new_a vs. hasattr(new_a, key)
     pprint.pprint(a)
     pprint.pprint(new_a)
+    print('test' in new_a)  # False
+    print(hasattr(new_a, 'test'))  # True
+    print('test' in new_a)  # now it's True
 
 if __name__ == '__main__':
     main()
--- pypanart.py
+++ pypanart.py
@@ -9,31 +9,40 @@ class SharedRuntimeState:
     pass
 
 def get_context_objects(state_file):
-        """Return (dict,object), being a persistent (JSON backed) dict and
-        a runtime only object, both being shared state for make.py doit tasks.
+    """Return (DefaultDotDict,DefaultDotDict), being a persistent (JSON
+    backed) and a runtime only object, both being shared state for make.py
+    doit tasks.
 
-        Typical usage:
+    Typical usage:
 
-            C, D = get_context_objects("/path/to/myproj.statefile.json")
-        """
-        if os.path.exists(state_file):
-            C = DefaultDotDict.json_load(open(state_file))
-        else:
-            C = DefaultDotDict()
-        C._metadata.run_at = time.asctime()
-        proc = Popen('git rev-parse HEAD'.split(), stdout=PIPE)
-        commit, _ = proc.communicate()
-        C._metadata.run_commit = commit.strip()
-        C._metadata.__filepath = state_file
-        
-        # doit inspects things looking for .create_doit_tasks and
-        # failes when C and D return {}, so add dummy method
-        C.create_doit_tasks = lambda: None
-        D = DefaultDotDict()
-        D.create_doit_tasks = C.create_doit_tasks
+        C, D = get_context_objects("/path/to/myproj.statefile.json")
+    """
+    if os.path.exists(state_file):
+        C = DefaultDotDict.json_load(open(state_file))
+    else:
+        C = DefaultDotDict()
+    C._metadata.run_at = time.asctime()
+    proc = Popen('git rev-parse HEAD'.split(), stdout=PIPE)
+    commit, _ = proc.communicate()
+    C._metadata.run_commit = commit.strip()
+    C._metadata.__filepath = state_file
 
-        return C, D
+    # doit inspects things looking for .create_doit_tasks and
+    # failes when C and D return {}, so add dummy method
+    C.create_doit_tasks = lambda: None
+    D = DefaultDotDict()
+    D.create_doit_tasks = C.create_doit_tasks
+
+    return C, D
 def run_with_context(func, C):
+    """Run func(), ensuring any changes to C are saved
+
+    Typical usage:
+
+        if __name__ == '__main__':
+            pypanart.run_with_context(main, C)
+    """
+
     try:
         C['_metadata']['run_failed'] = True
         func()
